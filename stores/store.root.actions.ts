f2d0126f30beff2997145b7b46fb6334612a7808
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2023-02-12 21:25:46 +0000

    Remove via splice rather than filter.

--- stores/store.root.actions.ts
+++ stores/store.root.actions.ts
@@ -1,12 +1,21 @@
 import { IRootStore, ISbItemObj } from "../types/types.rootStore";
 
+const removeItemFromArray = <T>(arr: Array<T>, obj: T): Array<T> => {
+  const index = arr.indexOf(obj);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+};
+
 export const rootActions = {
   addToArray(this: IRootStore, payload: ISbItemObj) {
-    let indexPayload = this.sbArray.findIndex((obj: ISbItemObj) => obj.name === payload.name);
-    if (indexPayload === -1) {
+    let indexOfPayload = this.sbArray.findIndex((obj: ISbItemObj) => obj.name === payload.name);
+    if (indexOfPayload === -1) {
       this.sbArray.push(payload);
     } else {
-      this.sbArray = this.sbArray.filter((obj: ISbItemObj) => obj.name !== payload.name);
+      // this.sbArray = this.sbArray.filter((obj: ISbItemObj) => obj.name !== payload.name);
+      this.sbArray = removeItemFromArray(this.sbArray, payload);
     }
   },
 };
