728e7e9424a12aae6691a59c1814d216f2515346
Simon <simon.cornforth@srcdev.co.uk>
2023-02-12 00:21:14 +0000

    Dev (#9)
    * Pinia stores added and Storybook integration
    
    * Added linters and vscode configs and extensions
    
    * Fix bad merge
    
    * Example story for updating array.
    
    * Add array add/remove example component

--- store-sample-actions/ArrayActions.vue
+++ store-sample-actions/ArrayActions.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <h1>Store array actions</h1>
+    <h1>Store array actions (Add/Remove object)</h1>
+    <section>
+      <h2>Store actions</h2>
+      <div>
+        <h3>
+          Add to array. &raquo; <button type="submit" @click.prevent="addToStore()">{{ buttonText }}</button>
+        </h3>
+      </div>
+    </section>
     <section>
       <h2>Display array <strong>sbArray</strong></h2>
       <div>
-        <template v-for="item in arrayToDisplay">
+        <template v-for="item in sbArray">
           <div>
             <p><strong>Name:</strong> {{ item.name }} | <strong>Description:</strong> {{ item.description }}</p>
           </div>
         </template>
       </div>
     </section>
-    <section>
-      <h2>Store actions</h2>
-      <div>
-        <h3>Add to array. &raquo; <button type="submit" @click.prevent="addToStore()">Add item</button></h3>
-      </div>
-    </section>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, type PropType } from "vue";
+import { ref, watch, type PropType } from "vue";
+import { useI18n } from 'vue-i18n'
+import { storeToRefs } from "pinia";
 import { useRootStore } from "../stores/store.root"; // Only need to import here due to lack of imports support within Storybook.
 import { ISbItemObj } from "../types/types.rootStore";
+
+const emit = defineEmits(["arrayUpdated"]);
 
 const { sbArrayItem } = defineProps({
   sbArrayItem: {
@@ -31,13 +37,24 @@ const { sbArrayItem } = defineProps({
     default: {},
   }
 });
+const { t } = useI18n()
+const buttonText = ref(t("store-sample-actions.array-actions.button-text-add"))
 
 const rootStore = useRootStore();
-const arrayToDisplay = reactive<ISbItemObj[]>(rootStore.sbArray);
+const { sbArray } = storeToRefs(rootStore);
 
 const addToStore = () => {
   rootStore.addToArray(sbArrayItem);
 }
+
+watch(
+  () => sbArray,
+  () => {
+    emit("arrayUpdated");
+    let indexPayload = sbArray.value.findIndex((obj: ISbItemObj) => obj.name === sbArrayItem.name);
+    buttonText.value = (indexPayload === -1) ? t("store-sample-actions.array-actions.button-text-add") : t("store-sample-actions.array-actions.button-text-remove");
+  }
+);
 </script>
 
 <style lang="scss">
