aecb11dc0a0c216f20aeaf10e109dfcbc1655262
Simon <simon.cornforth@srcdev.co.uk>
2023-02-21 22:32:06 +0000

    Added config in Storybook to set head items found in Nuxt.config (#22)
    * Readme updated - Explainer on store auto imports.
    
    * Added capability to set head items found in Nuxt.config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,33 @@
 import { addDecorator, app } from "@storybook/vue3";
+import { createHead } from "@vueuse/head";
 import { createI18n } from "vue-i18n";
 import translations from "../locales";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+import "@/assets/styles/index.scss";
+
+const head = createHead({
+  script: [
+    {
+      src: "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js",
+      crossorigin: "anonymous",
+    },
+    {
+      src: "https://kit.fontawesome.com/9d0a19ab04.js",
+      crossorigin: "anonymous",
+    },
+  ],
+  link: [
+    {
+      rel: "stylesheet",
+      type: "text/css",
+      href: "https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700%7CGreat+Vibes:400%7CRoboto:regular,700%7CDancing+Script:regular,500,600,700",
+      media: "all",
+    },
+  ],
+});
+app.use(head);
 
 /*
  * i118n set up
