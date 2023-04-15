192ccf6a96b8fcbacb4daa7763010de3b6512a14
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2023-04-15 22:37:39 +0100

    Storybook package updates, added vueuse for createHead

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,21 @@
 const path = require("path");
-// import { useRootStore } from "@/stores/store.root";
-
-module.exports = {
-  // stories: [
-  //   // "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  //   "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  //   "../layouts/**/*.stories.@(js|jsx|ts|tsx)",
-  //   "../pages/**/*.stories.@(js|jsx|ts|tsx)",
-  //   "../store-sample-actions/**/*.stories.@(js|jsx|ts|tsx)",
-  //   // TODO: Generic stories location
-  //   // "../**/*.stories.@(js|jsx|ts|tsx)",
-  // ],
+const config = {
   stories: ["../**/*.stories.ts"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
-  framework: "@storybook/vue3",
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
   features: {
     storyStoresV7: true,
   },
-  core: {
-    builder: "@storybook/builder-vite",
-  },
   async viteFinal(config, { configType }) {
-    config.base = "/src/";
+    // config.base = "/src/";
     config.resolve.alias = {
       ...config.resolve.alias,
       "@": path.resolve(__dirname, "./../"),
       "~": path.resolve(__dirname, "./../"),
     };
-
     if (configType === "DEVELOPMENT") {
       return {
         ...config,
@@ -37,7 +25,11 @@ module.exports = {
         },
       };
     }
-
     return config;
   },
+  docs: {
+    autodocs: true,
+  },
 };
+
+export default config;
