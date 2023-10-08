279d38e5a1958eaff45c40e64eb3743dc166d5fd
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2023-10-08 14:43:42 +0100

    Tooling updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,57 @@
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    shim: false,
+  },
+  devServer: {
+    https: true,
+    port: 3000,
+  },
   ssr: false,
-  css: ["modern-normalize", "~/assets/styles/index.scss"],
+  css: ['modern-normalize', '~/assets/styles/index.scss'],
   runtimeConfig: {
     // The private keys which are only available within server-side
-    apiSecret: "apiSecret from Nuxt.config",
+    apiSecret: 'apiSecret from Nuxt.config',
     // Keys within public, will be also exposed to the client-side
     public: {
-      apiBase: "/api-from-Nuxt.config",
+      apiBase: '/api-from-Nuxt.config',
     },
   },
-  modules: ["@pinia/nuxt", "nuxt-vitest"],
+  modules: ['@pinia/nuxt', 'nuxt-vitest'],
   components: [
     {
-      path: "~/components",
+      path: '~/components',
       pathPrefix: false,
     },
   ],
-  plugins: ["@/plugins/i18n"],
+  plugins: ['@/plugins/i18n'],
   app: {
     head: {
       htmlAttrs: {
-        lang: "en",
+        lang: 'en',
       },
-      titleTemplate: "%s - Website name",
+      titleTemplate: '%s - Website name',
       link: [
         {
-          rel: "icon",
-          type: "image/x-icon",
-          href: "./assets/images/favicon.jpg",
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: './assets/images/favicon.jpg',
         },
         // {
         //   rel: "stylesheet",
         //   href: "./node_modules/modern-normalize/modern-normalize.css",
         // },
       ],
-      meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
+      meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     },
     pageTransition: {
-      name: "page",
-      mode: "out-in",
+      name: 'page',
+      mode: 'out-in',
     },
     layoutTransition: {
-      name: "layout",
-      mode: "out-in",
+      name: 'layout',
+      mode: 'out-in',
     },
   },
 });
