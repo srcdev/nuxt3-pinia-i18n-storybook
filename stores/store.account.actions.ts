47b9dace071dc652b1358be9e7af29ffcda1654e
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2023-10-15 21:09:54 +0100

    Simulate sign in/out with fake api calls. Updates store for toggle of left nav

--- stores/store.account.actions.ts
+++ stores/store.account.actions.ts
@@ -2,6 +2,13 @@ import { IAccountState } from "@/types/types.accountStore";
 
 export const accountActions = {
   async updateLoginState(this: IAccountState, payload: boolean) {
+    if (this.signedIn) {
+      this.signOut(payload);
+    } else {
+      this.signIn(payload);
+    }
+  },
+  async signIn(this: IAccountState, payload: boolean) {
     await useFetch("/api/sign-in").then((response) => {
       this.signedIn = response.data.value;
       this.currentUser.name = "Test";
@@ -12,6 +19,17 @@ export const accountActions = {
       if (response.data) {
         cookieSignedIn.value = true;
       }
+    });
+  },
+  async signOut(this: IAccountState, payload: boolean) {
+    await useFetch("/api/sign-out").then((response) => {
+      this.signedIn = false;
+      this.currentUser.name = "";
+
+      const cookieSignedIn = useCookie(".AUTH", {
+        sameSite: true,
+      });
+      cookieSignedIn.value = null;
     });
   },
 };
