bdaf1fb555d3543e113e4a06c06c84d87dec73ca
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2024-01-29 22:03:30 +0000

    Added more icon buttons and style attrs for flex

---
+++ components/forms/input-button/IconButtonEyeClosed.vue
@@ -0,0 +1,42 @@
+<template>
+  <InputButtonCore type="button" :data-test-id="dataTestId" variant="icon-only" :size="size" :weight="weight" :button-text="buttonText" :style-class-passthrough="styleClassPassthrough" :button-text-visually-hidden="true">
+    <template #left>
+      <Icon name="radix-icons:eye-none" />
+    </template>
+  </InputButtonCore>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    size: {
+      type: String as PropType<string>,
+      default: "normal",
+      validator(value: string) {
+        return ["small", "normal", "medium", "large"].includes(value);
+      },
+    },
+    weight: {
+      type: String as PropType<string>,
+      default: "wght-500",
+      validator(value: string) {
+        return ["wght-100", "wght-200", "wght-300", "wght-400", "wght-500", "wght-600", "wght-700", "wght-800", "wght-900"].includes(value);
+      },
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    dataTestId: {
+      type: String,
+      default: "",
+    },
+    styleClassPassthrough: {
+      type: String,
+      default: "",
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  // @import "@/assets/styles/imports.scss";
+</style>
--- pages/samples/forms/sample-buttons.vue
+++ pages/samples/forms/sample-buttons.vue
@@ -12,90 +12,182 @@
         <PageRow :use-available-width="false" :apply-gutters="false" page-row-inner-theme="theme-white">
           <template #pageRowContent>
             <form>
-              <FlexGroup align-content="top-left" gap="12px" :full-width="true" style-class-passthrough="mt-20 mb-20">
-                <template #default>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal mb-12"><code>&lt;InputButton variant="primary"&gt;</code></p>
-                      <InputButton type="submit" variant="primary" button-text="Primary Small" size="small" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="primary" button-text="Primary Normal" size="normal" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="primary" button-text="Primary Medium" size="medium" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="primary" button-text="Primary Large" size="large" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="primary" button-text="Primary Large 100" size="large" weight="wght-100" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="primary" button-text="Primary Large 900" size="large" weight="wght-900" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal mb-12"><code>&lt;InputButton variant="secondary"&gt;</code></p>
-                      <InputButton type="submit" variant="secondary" button-text="Secondary Small" size="small" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="secondary" button-text="Secondary Normal" size="normal" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="secondary" button-text="Secondary Medium" size="medium" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="secondary" button-text="Secondary Large" size="large" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="secondary" button-text="Secondary Large 100" size="large" weight="wght-100" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="secondary" button-text="Secondary Large 900" size="large" weight="wght-900" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal mb-12"><code>&lt;InputButton variant="tertiary"&gt;</code></p>
-                      <InputButton type="submit" variant="tertiary" button-text="Tertiary Small" size="small" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="tertiary" button-text="Tertiary Normal" size="normal" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="tertiary" button-text="Tertiary Medium" size="medium" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="tertiary" button-text="Tertiary Large" size="large" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="tertiary" button-text="Tertiary Large 100" size="large" weight="wght-100" style-class-passthrough="mb-12" />
-                      <InputButton type="submit" variant="tertiary" button-text="Tertiary Large 900" size="large" weight="wght-900" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                </template>
-              </FlexGroup>
+              <div>
+                <p class="text-normal mb-12"><code>&lt;InputButton variant="primary"&gt;</code></p>
+                <div placement="left-center" gap="12px" flex-wrap="wrap">
+                  <div>
+                    <InputButton type="submit" variant="primary" button-text="Primary Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="primary" button-text="Primary Normal" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="primary" button-text="Primary Medium" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="primary" button-text="Primary Large" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="primary" button-text="Primary Large 100" size="large" weight="wght-100" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="primary" button-text="Primary Large 900" size="large" weight="wght-900" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
 
-              <FlexGroup align-content="top-left" gap="12px" :full-width="true" style-class-passthrough="mt-20 mb-20">
-                <template #default>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal mb-12"><code>&lt;InputButtonCancel&gt;</code></p>
-                      <InputButtonCancel type="submit" button-text="Cancel Small" size="small" style-class-passthrough="mb-12" />
-                      <InputButtonCancel type="submit" button-text="Cancel Normal" size="normal" style-class-passthrough="mb-12" />
-                      <InputButtonCancel type="submit" button-text="Cancel Medium" size="medium" style-class-passthrough="mb-12" />
-                      <InputButtonCancel type="submit" button-text="Cancel Large" size="large" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal mb-12"><code>&lt;InputButtonConfirm&gt;</code></p>
-                      <InputButtonConfirm type="submit" button-text="Confirm Small" size="small" style-class-passthrough="mb-12" />
-                      <InputButtonConfirm type="submit" button-text="Confirm Normal" size="normal" style-class-passthrough="mb-12" />
-                      <InputButtonConfirm type="submit" button-text="Confirm Medium" size="medium" style-class-passthrough="mb-12" />
-                      <InputButtonConfirm type="submit" button-text="Confirm Large" size="large" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal mb-12"><code>&lt;InputButtonWarning&gt;</code></p>
-                      <InputButtonWarning type="submit" button-text="Warning Small" size="small" style-class-passthrough="mb-12" />
-                      <InputButtonWarning type="submit" button-text="Warning Normal" size="normal" style-class-passthrough="mb-12" />
-                      <InputButtonWarning type="submit" button-text="Warning Medium" size="medium" style-class-passthrough="mb-12" />
-                      <InputButtonWarning type="submit" button-text="Warning Large" size="large" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                </template>
-              </FlexGroup>
+              <div>
+                <p class="text-normal mb-12"><code>&lt;InputButton variant="secondary"&gt;</code></p>
+                <div placement="left-center" gap="12px" flex-wrap="wrap">
+                  <div>
+                    <InputButton type="submit" variant="secondary" button-text="Secondary Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="secondary" button-text="Secondary Normal" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="secondary" button-text="Secondary Medium" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="secondary" button-text="Secondary Large" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="secondary" button-text="Secondary Large 100" size="large" weight="wght-100" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="secondary" button-text="Secondary Large 900" size="large" weight="wght-900" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
 
-              <FlexGroup align-content="top-left" gap="12px" :full-width="true" style-class-passthrough="mt-20 mb-20">
-                <template #default>
-                  <FlexGroupItem :flex-grow="false">
-                    <template #default>
-                      <p class="text-normal wght-700 mb-12">Icon Only Buttons</p>
-                      <p class="text-normal mb-12"><code>&lt;IconButtonCancel&gt;</code></p>
-                      <IconButtonCancel button-text="Cancel Small" size="small" style-class-passthrough="mb-12" />
-                      <IconButtonCancel button-text="Cancel Small" size="normal" style-class-passthrough="mb-12" />
-                      <IconButtonCancel button-text="Cancel Small" size="medium" style-class-passthrough="mb-12" />
-                      <IconButtonCancel button-text="Cancel Small" size="large" style-class-passthrough="mb-12" />
-                    </template>
-                  </FlexGroupItem>
-                </template>
-              </FlexGroup>
+              <div>
+                <p class="text-normal mb-12"><code>&lt;InputButton variant="tertiary"&gt;</code></p>
+                <div placement="left-center" gap="12px" flex-wrap="wrap">
+                  <div>
+                    <InputButton type="submit" variant="tertiary" button-text="Tertiary Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="tertiary" button-text="Tertiary Normal" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="tertiary" button-text="Tertiary Medium" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="tertiary" button-text="Tertiary Large" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="tertiary" button-text="Tertiary Large 100" size="large" weight="wght-100" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButton type="submit" variant="tertiary" button-text="Tertiary Large 900" size="large" weight="wght-900" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <p class="text-normal mb-12"><code>&lt;InputButtonCancel&gt;</code></p>
+                <div placement="left-center" gap="12px" flex-wrap="wrap">
+                  <div>
+                    <InputButtonCancel type="submit" button-text="Cancel Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonCancel type="submit" button-text="Cancel Normal" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonCancel type="submit" button-text="Cancel Medium" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonCancel type="submit" button-text="Cancel Large" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <p class="text-normal mb-12"><code>&lt;InputButtonConfirm&gt;</code></p>
+                <div placement="left-center" gap="12px" flex-wrap="wrap">
+                  <div>
+                    <InputButtonConfirm type="submit" button-text="Confirm Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonConfirm type="submit" button-text="Confirm Normal" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonConfirm type="submit" button-text="Confirm Medium" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonConfirm type="submit" button-text="Confirm Large" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <p class="text-normal mb-12"><code>&lt;InputButtonWarning&gt;</code></p>
+                <div placement="left-center" gap="12px" flex-wrap="wrap">
+                  <div>
+                    <InputButtonWarning type="submit" button-text="Warning Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonWarning type="submit" button-text="Warning Normal" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonWarning type="submit" button-text="Warning Medium" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <InputButtonWarning type="submit" button-text="Warning Large" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <p class="text-normal wght-700 mb-12">Icon Only Buttons</p>
+                <p class="text-normal mb-12"><code>&lt;IconButtonCancel&gt;</code></p>
+                <div placement="left-center" gap="12px">
+                  <div>
+                    <IconButtonCancel button-text="Cancel Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonCancel button-text="Cancel Small" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonCancel button-text="Cancel Small" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonCancel button-text="Cancel Small" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+
+                <p class="text-normal mb-12"><code>&lt;IconButtonEyeOpen&gt;</code></p>
+                <div placement="left-center" gap="12px">
+                  <div>
+                    <IconButtonEyeOpen button-text="Eye Open Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonEyeOpen button-text="Eye Open Small" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonEyeOpen button-text="Eye Open Small" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonEyeOpen button-text="Eye Open Small" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+
+                <p class="text-normal mb-12"><code>&lt;IconButtonEyeClosed&gt;</code></p>
+                <div placement="left-center" gap="12px">
+                  <div>
+                    <IconButtonEyeClosed button-text="Eye Closed Small" size="small" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonEyeClosed button-text="Eye Closed Small" size="normal" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonEyeClosed button-text="Eye Closed Small" size="medium" style-class-passthrough="mb-12" />
+                  </div>
+                  <div>
+                    <IconButtonEyeClosed button-text="Eye Closed Small" size="large" style-class-passthrough="mb-12" />
+                  </div>
+                </div>
+              </div>
             </form>
           </template>
         </PageRow>
